052d9472840d0bcd6e2e74e675837ddcd57d1e7a
Camcode17 <ayushivasanad2004@gmail.com>
2026-02-22 14:11:17 +0530

    Added Simple Calculator program

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+print("Note: The two numbers enered should be non zero integers")
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+print("\n Results")
+
+#Addition
+print(f"{num1}+{num2} = {num1 + num2}")
+#Subtraction
+print(f"{num1}-{num2} = {num1 - num2}")
+#Multiplication
+print(f"{num1}*{num2} = {num1 * num2}")
+#Division
+print(f"{num1}/{num2} = {num1 / num2}")
+#Modulus
+print(f"{num1}%{num2} = {num1 % num2}")
+#Exponentiation
+print(f"{num1}^{num2} = {num1 ** num2}")
