7d4de739ca509683ad9d2e67ccc882b5efdf1edb
Camcode17 <ayushivasanad2004@gmail.com>
2026-02-22 16:54:43 +0530

    added multiplication table prog

---
+++ multiplication_table_generator.py
@@ -0,0 +1,9 @@
+# Multiplication Table Generator
+
+number = int(input("Enter number: "))
+end = int(input("Enter range (end): "))
+
+print(f"\nMultiplication Table of {number}")
+
+for i in range(1, end + 1):
+    print(f"{number} x {i} = {number * i}")
